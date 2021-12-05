b8199230bb8cff3863d01dcc5d932f58770099bc
Fathi KADIR ABDALLAH <fathi.kadir.a@gmail.com>
2021-12-05 22:05:11 +0000

    Update AuthHelpers.jsx

--- src/helpers/AuthHelpers.jsx
+++ src/helpers/AuthHelpers.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-let a = document.location.toString().split(':')
-const AUTH_API_BASE_URL = a[0].concat(":4000").concat("/api/")
+let a = "http://".concat(document.location.hostname)
+const AUTH_API_BASE_URL = a.concat(":4000").concat("/api/")
 
 export const register = (username, password, color) => {
     return axios.post(AUTH_API_BASE_URL + "register", { username: username, password: password, color: color });
